703790212a632a68ab3bf3fb78a1db62e4fe6f70
VietTung <tungvip2143@gmail.com>
2022-05-10 16:06:59 +0700

    first commit

---
+++ homework 7/bai 1/main.js
@@ -0,0 +1,3 @@
+document.write(
+    "January<br> February<br> March<br> April<br> May<br> June<br> July<br> August<br> September<br> October<br> November<br> December"
+)
---
+++ homework 7/bai 3/main.js
@@ -0,0 +1,7 @@
+for (let i = 2; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+        document.write(i + "x" + j + "=" + (i * j))
+        document.write("<br/>");
+    }
+    document.write("<br/>");
+}
