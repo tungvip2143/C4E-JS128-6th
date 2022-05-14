6cdf393a603de3da04107b898ad677d79ddca8f6
VietTung <tungvip2143@gmail.com>
2022-05-14 14:55:10 +0700

    buoi 9

--- homework 9/main.js
+++ homework 9/main.js
@@ -26,21 +26,3 @@
 // );
 
 // BÀI 3:
-function validate() {
-    var u = document.getElementById("username").value;
-    var p1 = document.getElementById("password").value;
-
-
-    if (u == "") {
-        alert("Vui lòng nhập tên!");
-        return false;
-    }
-    if (p1 == "") {
-        alert("Vui lòng nhập mật khẩu!");
-        return false;
-    }
-
-    alert("Xin hãy điền đúng thông tin!")
-
-    return true;
-}
