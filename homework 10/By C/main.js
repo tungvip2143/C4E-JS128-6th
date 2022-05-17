4eb3f18de74bdf77ffd6db1bafb0acf1b67bfd5c
VietTung <tungvip2143@gmail.com>
2022-05-17 21:59:06 +0700

    buoi10

--- homework 10/By C/main.js
+++ homework 10/By C/main.js
@@ -1,21 +1,21 @@
 type = "text/javascript"
 
 function updateClock() {
-    var now = new Date();
-    var dname = now.getDay(),
+    let now = new Date();
+    let dname = now.getDay(),
         hou = now.getHours(),
         min = now.getMinutes(),
         sec = now.getSeconds()
 
     Number.prototype.pad = function(digits) {
         for (var n = this.toString(); n.length < digits; n = 0 + n);
-        return n
+        return n;
     }
 
-    var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var ids = ['dayname', 'hours', 'minutes', 'seconds']
-    var values = [week[dname], hou.pad(2), min.pad(2), sec.pad(2)];
-    for (var i = 0; i < ids.length; i++)
+    let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let ids = ['dayname', 'hours', 'minutes', 'seconds']
+    let values = [week[dname], hou.pad(2), min.pad(2), sec.pad(2)];
+    for (i = 0; i < ids.length; i++)
         document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
 
